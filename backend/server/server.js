c54f501f4fd317a4b57285e532eafea93b1a5f37
Anthrax <samgreilick@gmail.com>
2021-05-10 18:15:03 -0600

    testing server

--- backend/server/server.js
+++ backend/server/server.js
@@ -1,1 +1,12 @@
-const express = requre("express")
+const express = require("express");
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+// app.use(express.static("../../frontend"));
+app.use(express.static("./index.html"));
+
+app.listen(PORT,()=>{
+ console.log("PORT "+PORT+" has been overthrown");
+}); 
