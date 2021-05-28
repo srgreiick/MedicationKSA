ab03e481fab671d435b91e2b5f4d424f1926d047
Anthrax <samgreilick@gmail.com>
2021-05-28 15:39:19 -0600

    git ad .

--- backend/server/server.js
+++ backend/server/server.js
@@ -5,8 +5,6 @@ const PORT = process.env.PORT || 3000;
 const app = express();
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
-// app.use(express.static("../../frontend"));
-app.use(express.static("index.html"));
 
 // app.get("/", function(req, res) {
 //     res.send("Welcome to the Star Wars Page!");
