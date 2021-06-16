273a5f6c5acadc728f0a5934b1514dae8dc8d864
Anthrax <samgreilick@gmail.com>
2021-06-16 14:46:31 -0600

    server relocation for heroku

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 3000;
 const app = express();
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
-app.use(express.static("../frontend"));
+app.use(express.static("./frontend"));
 
 app.get("/", function(req, res){
     res.sendFile(path.join(__dirname, "index.html"));
