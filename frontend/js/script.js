b89f5626d867c1970d12f707aa9991d1c4507081
Anthrax <samgreilick@gmail.com>
2021-08-27 17:26:04 -0600

    fixed internal navigation

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -1,20 +1,20 @@
 
 console.log("Hello World");
 
-let medBtn = document.getElementById("medication_btn");
+// let medBtn = document.getElementById("medication_btn");
 
-medBtn.onclick = doSomething;
+// medBtn.onclick = doSomething;
 
-function doSomething(){
-    console.log("From Medication BTN");
-    window.location.href = "./html/medication.html";
-};
+// function doSomething(){
+//     console.log("From Medication BTN");
+//     window.location.href = "./html/medication.html";
+// };
 
-let practiceBtn = document.getElementById("practice_btn");
+// let practiceBtn = document.getElementById("practice_btn");
 
-practiceBtn.onclick = doSomething;
+// practiceBtn.onclick = doSomething;
 
-function doSomething(){
-    console.log("From Practice BTN");
-    // window.location.href = "./html/practice.html";
-};
+// function doSomething(){
+//     console.log("From Practice BTN");
+//     // window.location.href = "./html/practice.html";
+// };
