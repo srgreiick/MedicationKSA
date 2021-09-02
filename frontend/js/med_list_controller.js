46e8910ea19608e8c4f7cba676ceaeeb87e7522a
Anthrax <samgreilick@gmail.com>
2021-09-02 15:12:05 -0600

    comments to code

--- frontend/js/med_list_controller.js
+++ frontend/js/med_list_controller.js
@@ -1,7 +1,11 @@
+//test log
 console.log("From Med List Controller Line 1");
 
+//Call addRow function
 addRow();
 
+//Added content from js
+// function from https://stackoverflow.com/questions/17650776/add-remove-html-inside-div-using-javascript
 function addRow() {
     const div = document.createElement('div');
   
