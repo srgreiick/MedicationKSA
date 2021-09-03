958879a0ed3cecf7e4683458391c20b30d2f3124
Anthrax <samgreilick@gmail.com>
2021-09-02 18:44:11 -0600

    params added to item creator

--- frontend/js/med_list_controller.js
+++ frontend/js/med_list_controller.js
@@ -2,20 +2,20 @@
 console.log("From Med List Controller Line 1");
 
 
+let med = "Epinepherine 1:10"
+
 //Call addRow function
-addRow();
+addRow(med);
 
 
 //Create row for medication list
 // function from https://stackoverflow.com/questions/17650776/add-remove-html-inside-div-using-javascript
-function addRow() {
+function addRow(a) {
     const div = document.createElement('div');
   
     div.className = 'grid-item';
-  
-    div.innerHTML = `
-    <div>TEST</div>
-    `;
+    
+    div.innerHTML = "<div>"+a+"</div>";
   
     document.getElementById('grid-container').appendChild(div);
     console.log("From addRow Function in Med List Controller");
