c193ab5fde0e2f69984ee6d6ebba4622aa9f754e
Anthrax <samgreilick@gmail.com>
2021-09-08 15:37:36 -0600

    functional page generator with query strings

--- frontend/js/med_controller.js
+++ frontend/js/med_controller.js
@@ -1,1 +1,32 @@
+import med_list from "../database/med_list.js";
 console.log("From Med Controller line 1");
+
+let medication_f_list = window.location.search.substring(1)
+console.log(medication_f_list);
+
+let medication = med_list.med_list.meds
+
+
+
+for (let i = 0; i < medication.length; i++) {
+    if (medication[i].name == medication_f_list) {
+        console.log(medication[i]);
+        medication = medication[i]
+
+
+        addContent(medication) 
+    }
+    else{
+        console.log("nope");
+    }
+    
+}
+
+
+function addContent(a) {
+    const div = document.createElement('div');
+    div.innerHTML = "<div>"+a.name+"</div><div>"+a.amount+"</div>";
+  
+    document.getElementById('medication').appendChild(div);
+    console.log("From addRow Function in Med List Controller");
+};
